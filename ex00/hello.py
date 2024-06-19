a9ce899085c0d38f36b4afdee5189e0176b7997f
ShintaroSaeki <saeki@mur4l.com>
2024-06-19 13:07:56 +0900

    ADD: ex00

---
+++ ex00/hello.py
@@ -0,0 +1,13 @@
+ft_list  = ["Hello", "tata!"]
+ft_tuple = ("Hello", "Japan!")
+ft_set   = {"Hello", "Tokyo!"}
+ft_dict  = {"Hello" : "titi!"}
+
+#your code here
+ft_list[1] = "World!"
+ft_dict["Hello"] = "42Tokyo!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
